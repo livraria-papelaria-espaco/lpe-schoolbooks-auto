1e652b1f65e01dd29825e9bf565d11442d4a248e
Diogo Correia <me@diogotc.com>
2020-08-18 21:18:28 +0100

    Parse fetched data into clipboard
    Signed-off-by: Diogo Correia <me@diogotc.com>

--- src/components/Book.js
+++ src/components/Book.js
@@ -5,6 +5,68 @@ import React, { useState } from "react";
 
 const WOOK_REGEX = /<script type="application\/ld\+json">[^]*?({[^]+})[^]*?<\/script>[^]*?<!-- Fim Google/;
 
+const getPublisherData = (publisher) => {
+  switch (publisher) {
+    case "Porto Editora":
+      return { publisher: "", group: "pe" };
+    case "Areal Editores":
+      return { publisher: "areal", group: "pe" };
+    case "Raiz Editora / Lisboa Editora":
+      return { publisher: "raiz", group: "pe" };
+    case "Texto Editores":
+      return { publisher: "texto", group: "Leya" };
+    case "Edições Asa":
+      return { publisher: "asa", group: "Leya" };
+    case "Santillana":
+      return { publisher: "", group: "santillana" };
+    default:
+      return { publisher: "", group: publisher?.toLowerCase() || "" };
+  }
+};
+
+const getFormattedIsbn = (isbn, { publisher, group }) => {
+  if (group === "pe") {
+    if (publisher !== "") return "";
+    return isbn.split("-")[3] || "";
+  }
+  return isbn.replace(/-/g, "");
+};
+
+const parseBookData = ({ name, publisher, isbn }) => {
+  const nameSplit = name.split(" - ");
+
+  const typeIndex = nameSplit.findIndex((text) =>
+    [
+      "caderno de atividades",
+      "workbook",
+      "cahier d'exercices",
+      "cuaderno de ejercicios",
+    ].some((v) => text.toLowerCase().trim().includes(v))
+  );
+  const type = typeIndex === -1 ? "m" : "ca";
+  if (typeIndex !== -1) nameSplit.splice(typeIndex, 1);
+
+  const schoolYearIndex = nameSplit.findIndex((text) =>
+    text.trim().includes(".º Ano")
+  );
+  const schoolYear =
+    schoolYearIndex === -1
+      ? 0
+      : nameSplit[schoolYearIndex].trim().replace(".º Ano", "");
+  if (schoolYearIndex !== -1) nameSplit.splice(schoolYearIndex, 1);
+
+  const publisherData = getPublisherData(publisher);
+
+  return {
+    name: nameSplit[0] || "",
+    type,
+    schoolYear,
+    isbn,
+    ...publisherData,
+    formattedIsbn: getFormattedIsbn(isbn, publisherData),
+  };
+};
+
 const useStyles = makeStyles((theme) => ({
   root: {
     height: 200,
@@ -31,16 +93,25 @@ const Book = ({ img, url, title, author }) => {
         const response = await axios.get(url);
         const dataString = WOOK_REGEX.exec(response.data)?.[1] || [];
         const bookMetadata = JSON.parse(dataString);
-        stateData = {
+        console.log(bookMetadata);
+        stateData = parseBookData({
           name: bookMetadata.name,
-          author: bookMetadata.author?.name,
           publisher: bookMetadata.publisher?.name,
-        };
+          isbn: bookMetadata.isbn,
+        });
         setData(stateData);
       }
       console.log(stateData);
       await navigator.clipboard.writeText(
-        [stateData.name, stateData.author, stateData.publisher].join("\t")
+        [
+          "",
+          stateData.name,
+          stateData.schoolYear,
+          stateData.formattedIsbn,
+          stateData.type,
+          stateData.publisher,
+          stateData.group,
+        ].join("\t")
       );
       setSnackbarText("Texto copiado");
     } catch (e) {
